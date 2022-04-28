9b4f2474c3a226de770514734cea03b2e67fe9b8
Abylay <abyllight@gmail.com>
2022-04-28 18:54:47 +0600

    webhook fix

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use AmoCRM\Client;
 use App\Http\Resources\CuisineCollection;
 use App\Models\Week;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 
 class AdminController extends Controller
@@ -15,25 +16,30 @@ class AdminController extends Controller
             $amo = new Client(env('AMO_SUBDOMAIN'), env('AMO_LOGIN'), env('AMO_HASH'));
 
             $success = $amo->lead->apiList([
-                'limit_rows'=>500,
+                'limit_rows' => 500,
                 'status' => 142,
                 'date_create' => [
-                    'from' => 1626285600,
-                    'to' => 1627668000
+                    'from' => strtotime(Carbon::yesterday()), //strtotime(Carbon::yesterday())
+                    'to' => strtotime(Carbon::now()) //strtotime(Carbon::now())
                 ]
             ]);
-            return response()->json(count($success));
+
+            //return response()->json(count($success));
+
+            if (count($success) <= 0) {
+                return response()->json('Success');
+            }
             //стоимость курса 456321
             foreach ($success as $item){
                 $course = null;
 
-                foreach ($item['custom_fields'] as $field){
-                    if ($field['id'] === 456321){
+                foreach ($item['custom_fields'] as $field) {
+                    if ($field['id'] === 456321) {
                         $course = (int)$field["values"][0]["value"];
                     }
                 }
                 //dump($item['name'], $item['price'], $course);
-                if($course != null || $course == 0){
+                if($course != null || $course == 0) {
                     $lead = $amo->lead;
                     $lead['price'] = $course;
                     $lead->apiUpdate((int)$item['id']);
