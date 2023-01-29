860b24dad6b0311c273755d90d47284d9f55b990
Abylay <abyllight@gmail.com>
2023-01-29 17:05:27 +0600

    fix sizes

--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -45,12 +45,14 @@ class DishController extends Controller
 
         $dish->ingredients()->sync($request->ingredient_ids);
 
-        foreach ($request->sizes as $size) {
-            $ds = new DishSize();
-            $ds->dish_id = $dish->id;
-            $ds->size = $size['id'];
-            $ds->weight = $size['val'];
-            $ds->save();
+        if ($request->sizes) {
+            foreach ($request->sizes as $size) {
+                $ds = new DishSize();
+                $ds->dish_id = $dish->id;
+                $ds->size = $size['id'];
+                $ds->weight = $size['val'];
+                $ds->save();
+            }
         }
 
         return response()->json([
