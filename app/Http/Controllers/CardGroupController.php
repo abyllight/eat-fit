72859840abf4dd16a6cd68b1ba8e7bcfc280db97
Abylay <abyllight@gmail.com>
2023-03-20 16:45:41 +0600

    fix mix pix

--- app/Http/Controllers/CardGroupController.php
+++ app/Http/Controllers/CardGroupController.php
@@ -69,7 +69,7 @@ class CardGroupController extends Controller
         }
 
         foreach ($user->groups as $i => $group) {
-            $selects = $group->selects->orderBy('code')->groupBy('code');
+            $selects = $group->selects()->orderBy('code')->groupBy('code');
 
             $groups[$i] = [
                 'id' => $group->id,
