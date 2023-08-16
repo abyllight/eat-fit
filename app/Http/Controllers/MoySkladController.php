b365e74610f92e1672e34489d85360ff0f416f95
Abylay <abyllight@gmail.com>
2023-08-16 13:43:30 +0600

    moysklad fix7 get events

--- app/Http/Controllers/MoySkladController.php
+++ app/Http/Controllers/MoySkladController.php
@@ -18,7 +18,7 @@ class MoySkladController extends Controller
 
     public function doWebhook(Request $request) {
         $url = $request->query('requestId');
-        $type = $request->collect();
+        $type = $request->get('events');
 
         $moy_sklad = new MoySklad();
         $moy_sklad->url = $url;
